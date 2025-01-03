8f13e59e615d2c6cb35ed5641e6b99f5e9a91847
christianmarrtinez <christianmarrtinez@gmail.com>
2025-01-02 22:24:23 -0500

    Add dropdown for the category section in create listing form

--- auctions/forms.py
+++ auctions/forms.py
@@ -10,5 +10,5 @@ class AuctionListingForm(forms.ModelForm):
             'description': forms.Textarea(attrs={'class': 'form-control'}),
             'starting_bid': forms.NumberInput(attrs={'class': 'form-control'}),
             'image_url': forms.URLInput(attrs={'class': 'form-control'}),
-            'category': forms.TextInput(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=AuctionListing.CATEGORY_CHOICES),
         }
